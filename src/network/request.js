793cd5a076d85eb389e36ac15c6f6c51aaaf67c3
pyj-pengpeng <zjypyj0216@163.com>
2020-06-24 17:23:15 +0800

    '首页'

---
+++ src/network/request.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+export function request(config) {  //函数返回的是promise，可以直接用then
+  // 1.创建axios的实例
+  const instance = axios.create({
+    baseURL: 'http://152.136.185.210:8000/api/n3',
+    timeout: 5000
+  })
+
+  // 2.axios的拦截器
+  // 2.1请求拦截的作用
+  instance.interceptors.request.use(config => {
+    return config;
+  },err => {
+    // console.log(err);
+  })
+
+  // 3.发送真正的网络请求
+  return instance(config)
+}
---
+++ src/views/home/Home.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles="['流行', '新款', '精选']" class="tab-control"></tab-control>
+
+    <ul>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+
+
+
+
+
+
+
+
+
+    </ul>
+  </div>
+</template>
+
+<script>
+  import HomeSwiper from "./childComps/HomeSwiper";
+  import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
+
+  import NavBar from "../../components/common/navbar/NavBar";
+  import TabControl from "components/content/tabControl/TabControl";
+
+  import {getHomeMultidata} from "../../network/home";
+
+
+  export default {
+    name: "Home",
+    components: {
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      NavBar,
+      TabControl
+    },
+    data() {
+        return {
+          banners: [],
+          recommends: []
+        }
+    },
+    //组件一旦创建好就立刻发送网络请求，请求数据
+    created() {
+        // 1/请求多个数据
+      getHomeMultidata().then(res => {
+        console.log(res);
+        this.banners = res.data.data.banner.list;
+        this.recommends = res.data.data.recommend.list;
+      })
+    }
+  }
+</script>3000047
+
+<style scoped>
+  #home {
+    padding-top: 44px;
+  }
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
+  }
+</style>
